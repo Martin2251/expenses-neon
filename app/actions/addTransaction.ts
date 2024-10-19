c39f8835570cf0fe970ed3683ca841292d0ffca9
Martin Lowe <martinlowe673@gmail.com>
2024-10-19 14:52:40 +0100

    addtransaction

--- app/actions/addTransaction.ts
+++ app/actions/addTransaction.ts
@@ -15,4 +15,13 @@ interface TransactionResult{
 async function addTransaction (formData:FormData): Promise <TransactionResult>{
     const textValue = formData.get('text')
     const amountValue = formData.get('amount')
+
+    //Check for input values
+
+    if(!textValue || textValue === '' || !amountValue){
+        return {error: 'Please enter both text and amount'}
+    }
+
+    const text:string = textValue.toString();
+    const amount:number = parseFloat(amountValue.toString());
 }
