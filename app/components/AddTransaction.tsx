fc40b2601d10a37b6c30faf017e1d8c93bef2a65
Martin Lowe <martinlowe673@gmail.com>
2024-10-19 14:40:02 +0100

    add transaction

--- app/components/AddTransaction.tsx
+++ app/components/AddTransaction.tsx
@@ -1,11 +1,16 @@
+"use client"
 import React from 'react'
 
 const AddTransaction = () => {
+
+    const clientAction = async(formData:FormData) =>{
+        console.log(formData.get('text'), formData.get('amount'))
+    }
   return (
   <>
   <h3>Add Transaction</h3>
-  <form action="">
-    
+  <form action={clientAction}>
+
   </form>
   </>
   )
