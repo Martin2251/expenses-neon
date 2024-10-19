f690f499299ce7c7b1bcdad3c79b0995234eb146
Martin Lowe <martinlowe673@gmail.com>
2024-10-19 14:36:47 +0100

    guest

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,18 @@
 import React from 'react'
+import Guest from './components/Guest'
+import {currentUser} from "@clerk/nextjs/server"
 
-const HomePage = () => {
+
+
+const HomePage = async () => {
+
+  const user = await currentUser()
+  if(!user) {
+    return <Guest />
+  }
   return (
    <main>
-    <h1>expense tracker</h1>
+    <h1>Welcome, {user.firstName}</h1>
    </main>
   )
 }
