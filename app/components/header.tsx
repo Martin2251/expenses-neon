e4217dbb1e691460c914a780fd91b034d6523d50
Martin Lowe <martinlowe673@gmail.com>
2024-10-19 14:16:30 +0100

    header

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -1,10 +1,14 @@
 import React from 'react'
+import {SignInButton, SignedIn,SignedOut,UserButton} from "@clerk/nextjs"
 
 const Header = () => {
   return (
     <nav className='navbar'>
         <div className='navbar-container'>
-            
+            <h2>Expense Tracker</h2>
+            <div>
+
+            </div>
         </div>
     </nav>
   )
