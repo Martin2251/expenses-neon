24f3cd19f1cd13bf169c51d454688041efc2a456
Martin Lowe <martinlowe673@gmail.com>
2024-10-19 14:42:57 +0100

    add transaction

--- app/components/AddTransaction.tsx
+++ app/components/AddTransaction.tsx
@@ -10,6 +10,15 @@ const AddTransaction = () => {
   <>
   <h3>Add Transaction</h3>
   <form action={clientAction}>
+    <div className='form-control'>
+        <label htmlFor='text'>Text</label>
+        <input type='text' id='text' name='text' placeholder='enter text...'/>
+    </div>
+    <div className='form-control'>
+        <label htmlFor='amount'>Amount <br /> (negative - expense, postive - income)</label>
+        <input type='number' id='amount' name='amount' placeholder='enter amount...'  />
+
+    </div>
 
   </form>
   </>
