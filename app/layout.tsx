e3a6e540bea1ded963b1fda73835de68b5ae6771
Martin Lowe <martinlowe673@gmail.com>
2024-10-19 17:00:13 +0100

    toastify

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,8 @@ import './globals.css'
 import { Roboto } from 'next/font/google'
 import { ClerkProvider } from '@clerk/nextjs'
 import Header from './components/header'
+import { ToastContainer } from 'react-toastify'
+import "react-toastify/dist/reactToastify.css"
 
 const roboto = Roboto({ weight:"400",subsets: ['latin'] })
 
