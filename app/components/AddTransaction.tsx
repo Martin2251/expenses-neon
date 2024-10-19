de7f9620a516bb0e7218c30e72a7b9a87df2e1e5
Martin Lowe <martinlowe673@gmail.com>
2024-10-19 14:38:04 +0100

    add transaction

---
+++ app/components/AddTransaction.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const AddTransaction = () => {
+  return (
+  <>
+  <h3>Add Transaction</h3>
+  <form action="">
+    
+  </form>
+  </>
+  )
+}
+
+export default AddTransaction
