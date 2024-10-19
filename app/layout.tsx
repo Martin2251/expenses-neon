ead8c003fa2b298d4e9b4fc8684bea6d0a12c1dc
Martin Lowe <martinlowe673@gmail.com>
2024-10-19 14:14:46 +0100

    clerk provider

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import './globals.css'
 import { Roboto } from 'next/font/google'
+import { ClerkProvider } from '@clerk/nextjs'
 
 const roboto = Roboto({ weight:"400",subsets: ['latin'] })
 
@@ -14,8 +15,10 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
+    <ClerkProvider>
     <html lang="en">
       <body className={roboto.className}>{children}</body>
     </html>
+    </ClerkProvider>
   )
 }
