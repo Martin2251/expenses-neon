b72ae27ca25e471f0fd9996616ad32e1e5e11c8d
Martin Lowe <martinlowe673@gmail.com>
2024-10-19 14:15:39 +0100

    header

---
+++ app/components/header.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <nav className='navbar'>
+        <div className='navbar-container'>
+            
+        </div>
+    </nav>
+  )
+}
+
+export default Header
