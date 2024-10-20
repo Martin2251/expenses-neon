5d11387ccb1117838cd9e2a4310ab269c84c6c06
Martin Lowe <martinlowe673@gmail.com>
2024-10-20 18:13:19 +0100

    now better

--- app/actions/addTransaction.ts
+++ app/actions/addTransaction.ts
@@ -1,26 +1,53 @@
-"use server"
-import { db } from "@/lib/db"
-import { auth } from "@clerk/nextjs/server"
-import { Trykker } from "next/font/google";
+'use server';
+import { auth } from '@clerk/nextjs/server';
+import { db } from '@/lib/db';
+import { revalidatePath } from 'next/cache';
 
-async function getUserBalance(): Promise<{
-    balance?: number,
-    error?:string,
-}>{
-    const {userId} = auth();
-    if(!userId){
-        return {error:"user not found"}
-    }
-
-    try {
-        const transactions = await db.transaction.findMany({
-            where:{userId}
-        })
-        const balance = transactions.reduce((sum,transaction) =>sum + transaction.amount,0)
-        return {balance}
-    } catch (error) {
-        return {error:"database error"}
-    }
+interface TransactionData {
+  text: string;
+  amount: number;
 }
 
-export default getUserBalance
+interface TransactionResult {
+  data?: TransactionData;
+  error?: string;
+}
+
+async function addTransaction(formData: FormData): Promise<TransactionResult> {
+  const textValue = formData.get('text');
+  const amountValue = formData.get('amount');
+
+  // Check for input values
+  if (!textValue || textValue === '' || !amountValue) {
+    return { error: 'Text or amount is missing' };
+  }
+
+  const text: string = textValue.toString(); // Ensure text is a string
+  const amount: number = parseFloat(amountValue.toString()); // Parse amount as number
+
+  // Get logged in user
+  const { userId } = auth();
+
+  // Check for user
+  if (!userId) {
+    return { error: 'User not found' };
+  }
+
+  try {
+    const transactionData: TransactionData = await db.transaction.create({
+      data: {
+        text,
+        amount,
+        userId,
+      },
+    });
+
+    revalidatePath('/');
+
+    return { data: transactionData };
+  } catch (error) {
+    return { error: 'Transaction not added' };
+  }
+}
+
+export default addTransaction;
--- app/components/AddTransaction.tsx
+++ app/components/AddTransaction.tsx
@@ -1,41 +1,51 @@
-"use client"
-import React from 'react'
-import addTransaction from '../actions/addTransaction'
-import {toast} from "react-toastify"
-import { useRef } from 'react'
+'use client';
+import { useRef } from 'react';
+import addTransaction from '@/app/actions/addTransaction';
+import { toast } from 'react-toastify';
 
 const AddTransaction = () => {
-    const formRef = useRef<HTMLFormElement>(null)
+  const formRef = useRef<HTMLFormElement>(null);
 
-    const clientAction = async(formData:FormData) =>{
-        const {data,error} = await addTransaction(formData);
+  const clientAction = async (formData: FormData) => {
+    const { data, error } = await addTransaction(formData);
 
-        if(error){
-            toast.error(error)
-        }else{
-            toast.success('transaction added')
-            formRef.current?.reset();
-            }
-        }
-    
+    if (error) {
+      toast.error(error);
+    } else {
+      toast.success('Transaction added');
+      formRef.current?.reset();
+    }
+  };
+
   return (
-  <>
-  <h3>Add Transaction</h3>
-  <form ref={formRef}action={clientAction}>
-    <div className='form-control'>
-        <label htmlFor='text'>Text</label>
-        <input type='text' id='text' name='text' placeholder='enter text...'/>
-    </div>
-    <div className='form-control'>
-        <label htmlFor='amount'>Amount <br /> (negative - expense, postive - income)</label>
-        <input type='number' id='amount' name='amount' placeholder='enter amount...' step="0.01" />
+    <>
+      <h3>Add transaction</h3>
+      <form ref={formRef} action={clientAction}>
+        <div className='form-control'>
+          <label htmlFor='text'>Text</label>
+          <input
+            type='text'
+            id='text'
+            name='text'
+            placeholder='Enter text...'
+          />
+        </div>
+        <div className='form-control'>
+          <label htmlFor='amount'>
+            Amount <br /> (negative - expense, positive - income)
+          </label>
+          <input
+            type='number'
+            name='amount'
+            id='amount'
+            placeholder='Enter amount...'
+            step='0.01'
+          />
+        </div>
+        <button className='btn'>Add transaction</button>
+      </form>
+    </>
+  );
+};
 
-    </div>
-    <button className='btn'>Add Transaction</button>
-
-  </form>
-  </>
-  )
-}
-
-export default AddTransaction
+export default AddTransaction;
