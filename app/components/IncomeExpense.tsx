7fab06063816a868df3f8d684aa88cf4fe8e9666
Martin Lowe <martinlowe673@gmail.com>
2024-10-20 16:30:50 +0100

    expenses

--- app/components/IncomeExpense.tsx
+++ app/components/IncomeExpense.tsx
@@ -5,6 +5,10 @@ const IncomeExpense = () => {
     <div className='inc-exp-container'>
         <div>
             <h4>Income</h4>
+            <p className='money plus'>$700</p>
+        </div>
+        <div>
+            <h4>Expenses</h4>
         </div>
       
     </div>
