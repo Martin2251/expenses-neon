90fbca0bb4636fda73d8118ec728b68bf987d860
Martin Lowe <martinlowe673@gmail.com>
2024-10-20 17:18:42 +0100

    transaction item

--- app/components/TransactionList.tsx
+++ app/components/TransactionList.tsx
@@ -1,13 +1,21 @@
 import React from 'react'
 import { Transaction } from '@/types/Transaction'
+import getTransactions from '../actions/getTransactions'
+import TransactionItem from './TransactionItem'
 
-const TransactionList = () => {
+
+const TransactionList = async() => {
+    const {transactions,error} = await getTransactions();
+
+    if(error){
+        return<p className='error'>{error}</p>
+    }
   return (
   <>
   <h3>History</h3>
   <ul className='list'>
     {transactions && transactions.map((transaction: Transaction) =>(
-        <p>{transaction.text}</p>
+        <TransactionItem key={transaction.id} transaction={transaction} />
     ))}
   </ul>
   </>
