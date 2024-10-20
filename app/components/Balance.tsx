6d13ffdcc75bc0b8109d07caf0814bc641b6e88c
Martin Lowe <martinlowe673@gmail.com>
2024-10-20 16:28:25 +0100

    balance

--- app/components/Balance.tsx
+++ app/components/Balance.tsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import getUserBalance from '../actions/getUserBalance'
+import { addCommas } from '@/lib/utils';
 
 const Balance = async() => {
     const {balance} = await getUserBalance();
   return (
    <>
    <h4>Your balance</h4>
-   <h1>${balance ?? 0}</h1>
+   <h1>${addCommas(balance ?? 0)}</h1>
    </>
   )
 }
