7056029a4ab9f72abf9e95adea173362e0be546a
Martin Lowe <martinlowe673@gmail.com>
2024-10-20 16:29:38 +0100

    income

---
+++ app/components/IncomeExpense.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const IncomeExpense = () => {
+  return (
+    <div className='inc-exp-container'>
+        <div>
+            <h4>Income</h4>
+        </div>
+      
+    </div>
+  )
+}
+
+export default IncomeExpense
