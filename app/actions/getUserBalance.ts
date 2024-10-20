cbdcf384057430e1bdadc907e8a34e2221a0a017
Martin Lowe <martinlowe673@gmail.com>
2024-10-20 16:12:29 +0100

    try catch

--- app/actions/getUserBalance.ts
+++ app/actions/getUserBalance.ts
@@ -1,8 +1,20 @@
 "use server"
 import { db } from "@/lib/db"
 import { auth } from "@clerk/nextjs/server"
+import { Trykker } from "next/font/google";
 
 async function getUserBalance(): Promise<{
     balance?: number,
     error?:string,
-}>
+}>{
+    const {userId} = auth();
+    if(!userId){
+        return {error:"user not found"}
+    }
+
+    try {
+        
+    } catch (error) {
+        
+    }
+}
